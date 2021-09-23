0abb99ef2c3162aff71fc90f31b3c2258558ecff
Oluwayemisi Soremekun <oluwayemisi.soremekun@gmail.com>
2021-09-22 17:56:29 -0700

    commit tree 012ba00cd8fc039def5a6600dc12cbc93e793bca

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution here
+const myDrivers = ["Ladd", "Jemma", "Shawnte", "Mistique", "Jemma", "Vik", "Jemma"]
+const myDriversObject = [
+    {name:"Ladd",hometown: "Ife"}, 
+    {name:"Jemma", hometown: "Ife"}, 
+    {name:"Shawnte", hometown: "Abk"}, 
+    {name:"Mistique", hometown: "Ilesha"}, 
+    {name:"Jemma", hometown: "Ife"}, 
+    {name:"Vik", hometown: "Cali"}, 
+    {name:"Jemma", hometown: "Ibadan"}]
+
+function findMatching(array, string){
+    let newArray = array.filter((element) => {
+        if (element.toLowerCase() === string.toLowerCase())
+            return element
+    })
+    return newArray
+}
+
+function fuzzyMatch(array, string){
+    let newArray = array.filter(function (element){
+        if (element.startsWith(string))
+            return element
+    })
+    return newArray
+}
+
+function matchName(array, str){
+    let newArray = array.filter(element => {
+        if (element.name === str)
+            return element
+    })
+    return newArray
+}
